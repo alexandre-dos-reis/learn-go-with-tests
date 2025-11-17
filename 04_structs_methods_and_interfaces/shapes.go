250092fb46cbc17205f3d04dcdd4d1dda8d1de6b
Alexandre Dos Reis <ajm.dosreis.daponte@gmail.com>
2025-11-17 15:00:23 +0100

    struct and interfaces

---
+++ 04_structs_methods_and_interfaces/shapes.go
@@ -0,0 +1,42 @@
+package structsmethodsandinterfaces
+
+import "math"
+
+// Rectangle
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+// Circle
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+// Triangle
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height * 0.5
+}
+
+// Everytime you need to establish a communication between unrelated types, use an interface.
+// In Golang, Interface are implicit as long as types implements it.
+type Shape interface {
+	Area() float64
+}
---
+++ 04_structs_methods_and_interfaces/shapes_test.go
@@ -0,0 +1,34 @@
+package structsmethodsandinterfaces
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rect := Rectangle{Width: 10.0, Height: 10.0}
+	got := rect.Perimeter()
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rect := Rectangle{Width: 12.0, Height: 6.0}
+		checkArea(t, rect, 72.0)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
+	})
+}
